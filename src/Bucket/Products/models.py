ea83ef469b2c066b4640b8c9749f9ea6909aae72
vipulyadav150 <vipulyadav150@gmail.com>
2018-08-06 06:56:48 +0530

    Added the custom model manager ProductManager class to override the queries

--- src/Bucket/Products/models.py
+++ src/Bucket/Products/models.py
@@ -17,13 +17,26 @@ def upload_image_path(instance , filename):
 
 
 
+#Custom model managers
+class ProductManager(models.Manager):
+    def get_by_id(self,id):
+        #Override the custom get_wueryset(0 method and return results as you want using filter or get methods
+        # return self.get_queryset().filter(id=id)
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+    #you can also override the default methods like all() or filter()
+    # def all(self):
+    #     return self.blahblahblah
 
 class Product(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=20,default=39.99)
     image = models.ImageField(upload_to=upload_image_path,null=True,blank=True)
-
+    #Winding up the custom model manager objects (or give it another name rather than objects)
+    objects = ProductManager()
     def __str__(self):
         return self.title
 
--- src/Bucket/Products/views.py
+++ src/Bucket/Products/views.py
@@ -9,6 +9,11 @@ class ProductListView(ListView):
     template_name = 'products/list.html'
     print(queryset)
 
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        return Product.objects.filter(pk=pk)
+
     #To grab context data we will override get_context_data() method
 
     # def get_context_data(self, *args, **kwargs):
@@ -27,16 +32,35 @@ def product_list_view(r):
 
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
+    #Above queryset commented to apply the custom Product Manager model manager to class based detail view
     template_name = 'products/details.html'
     def get_context_data(self, *args , **kwargs):
         context = super(ProductDetailView , self).get_context_data(*args,**kwargs)
         print(context)
         return  context
 
-
-
-
+    #Setting custom model managers into class based views
+    #Override the get_object method
+    #Steps:
+        #Override the get_object method and pass *args and **kwargs as parameters
+        #get the request
+        #get the id
+        #get the required instance using custom model manager statement
+        # return instance
+    def get_object(self, *args,**kwargs):
+        request  = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404('OOPS! No Product Here!')
+        return instance
+    #instead of above get_object() we can use directly the get_queryset() method
+    # def get_queryset(self,*args,**kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
+    #THIS IS get_queryset() can be now applied to class based listview also
 
 def product_detail_view(r,pk=None,*args,**kwargs):
     # instance = Product.objects.get(pk=pk)
@@ -55,16 +79,22 @@ def product_detail_view(r,pk=None,*args,**kwargs):
     #     print('Huh!')
 
             #or
-    qs = Product.objects.filter(id=pk)
+    # qs = Product.objects.filter(id=pk)
+
     #The below queryset will fetch the list containing the product ..
     #Since the product is only one the list will have only one item
     #So qs.first() is tom assign the item as an insatnce ..similar to qs[0]
+            #or setup a custom model manager instead of above code to directly pick
+            #up the required queryset
+    instance = Product.objects.get_by_id(pk)
 
-    if qs.exists() and qs.count()==1:
-        instance = qs.first()
-    else:
+    #Adjusted the below code in custom model manager ProductManager
+    # if qs.exists() and qs.count()==1:
+    #     instance = qs.first()
+    # else:
+    #     raise Http404('OOPS! Sorry No Product Here!')
+    if instance is None:
         raise Http404('OOPS! Sorry No Product Here!')
-
     context = {
         'object':instance
     }
